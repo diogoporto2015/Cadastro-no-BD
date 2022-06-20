83c160ff2f0e0aeb69451b4b720ce83b6bef55c4
DiogoPS <71047847+diogoporto2015@users.noreply.github.com>
2022-06-20 15:06:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var btnEntrar = document.querySelector("#entrar");
+var btnVoltar = document.querySelector("#voltar");
+
+var body = document.querySelector("body");
+
+btnEntrar.addEventListener("click", function () {
+    body.className = "entrar-js";
+});
+
+btnVoltar.addEventListener("click", function() {
+    body.className = "voltar-js";
+})
+
+
